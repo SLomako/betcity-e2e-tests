fac7bd411a1e005aac543219f1d04baa34b5fd0f
Slomako <lomakosv@gmail.com>
2023-04-22 11:27:46 +0300

    ref 21.04

--- src/test/java/ru/lomakosv/page/components/UserInfoComponent.java
+++ src/test/java/ru/lomakosv/page/components/UserInfoComponent.java
@@ -9,13 +9,14 @@ import static io.qameta.allure.Allure.step;
 
 public class UserInfoComponent {
 
+    String fff = "15821548";
     private final SelenideElement
             VERIFICATION_NAME_LOGIN = $("[class='user-info__item-step-value']"),
             LOG_OUT_BUTTON = $("[class*=icon_logout-light]");
 
     public void verifyLogin() {
         step("Проверка номера счета", () -> {
-            VERIFICATION_NAME_LOGIN.shouldHave(Condition.text("15821548"));
+            VERIFICATION_NAME_LOGIN.shouldHave(Condition.text(fff));
         });}
 
     public void logOut() {
