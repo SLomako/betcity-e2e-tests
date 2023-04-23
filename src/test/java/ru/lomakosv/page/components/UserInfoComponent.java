c5eb538efde64d1a3ca2f651b4e0046f009b0448
Slomako <lomakosv@gmail.com>
2023-04-23 19:01:18 +0300

    ref 21.04

--- src/test/java/ru/lomakosv/page/components/UserInfoComponent.java
+++ src/test/java/ru/lomakosv/page/components/UserInfoComponent.java
@@ -2,14 +2,8 @@ package ru.lomakosv.page.components;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
-import org.aeonbits.owner.ConfigFactory;
-import ru.lomakosv.config.VerifyLogon;
-
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import ru.lomakosv.config.AuthAccountNumberConfig;
+import ru.lomakosv.config.ConfigurationManager;
 
 import static com.codeborne.selenide.Condition.enabled;
 import static com.codeborne.selenide.Selenide.$;
@@ -17,21 +11,17 @@ import static io.qameta.allure.Allure.step;
 
 public class UserInfoComponent {
 
+    public static AuthAccountNumberConfig authAccountNumberConfig  = ConfigurationManager.getAuthAccountNumberConfig();
 
     private final SelenideElement
             VERIFICATION_NAME_LOGIN = $("[class='user-info__item-step-value']"),
             LOG_OUT_BUTTON = $("[class*=icon_logout-light]");
 
-    public void verifyLogin()  throws IOException  {
-            String content = "verify=secret";
-            Path propsPath = Paths.get("src/test/resources/verify.properties");
-            Files.writeString(propsPath, content);
+    public void verifyLogin()  {
 
-        VerifyLogon verifyLogon = ConfigFactory.create(VerifyLogon.class, System.getProperties());
         step("Проверка номера счета", () -> {
-            VERIFICATION_NAME_LOGIN.shouldHave(Condition.text(verifyLogon.verify()));
+            VERIFICATION_NAME_LOGIN.shouldHave(Condition.text(authAccountNumberConfig.accountNumber()));
         });
-            Files.delete(propsPath);
     }
 
     public void logOut() {
