b8049588ece22bdbe14d4d1184fd2fb93762cf17
Slomako <lomakosv@gmail.com>
2023-04-25 17:57:04 +0300

    ref 25.04

--- src/test/java/ru/lomakosv/config/ProjectConfiguration.java
+++ src/test/java/ru/lomakosv/config/ProjectConfiguration.java
@@ -24,7 +24,7 @@ public class ProjectConfiguration {
     }
     public void selenoidVideo() {
         String remoteUrl = webConfig.remoteUrl();
-        Configuration.remote = "https://" + authSelenoidConfig.remote_username() + authSelenoidConfig.remote_password() + "@" + remoteUrl  + "/wd/hub";
+        Configuration.remote = "https://" + authSelenoidConfig.remote_username() + ":" + authSelenoidConfig.remote_password() + "@" + remoteUrl  + "/wd/hub";
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
                 "enableVNC", true,
