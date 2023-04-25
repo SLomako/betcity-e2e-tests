07430115644887fe40f95b147c576698ca3eb374
Slomako <lomakosv@gmail.com>
2023-04-25 22:37:39 +0300

    ref 25.04

--- src/test/java/ru/lomakosv/config/WebConfig.java
+++ src/test/java/ru/lomakosv/config/WebConfig.java
@@ -3,7 +3,6 @@ package ru.lomakosv.config;
 import org.aeonbits.owner.Config;
 
 @Config.Sources({
-        "system:properties",
         "classpath:${env}.properties"}
 )
 public interface WebConfig extends Config {
